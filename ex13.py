9d94bb08ee3f5c726ed6e3337837bb3311b12412
Nathanboss5000 <drumingboy@outlook.com>
2015-03-12 18:46:12 -0400

    Done! ex14.py

---
+++ ex13.py
@@ -0,0 +1,8 @@
+"from sys import argv"
+
+"script, first, second, thrid = argv"
+
+print "The script is called:", "script"
+print "Your first varible is:", "first"
+print "Your second varible is:", "second"
+print "Your thrid valible is:", "third"
