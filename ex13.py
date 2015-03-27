f245c1761fcb543cdf73592b72eb2f55de29d0e6
Nathanboss5000 <drumingboy@outlook.com>
2015-03-26 21:32:02 -0400

    done did i do it right

--- ex13.py
+++ ex13.py
@@ -1,8 +1,8 @@
-"from sys import argv"
+from sys import argv
 
-"script, first, second, thrid = argv"
+script, first, second, thrid = argv
 
-print "The script is called:", "script"
-print "Your first varible is:", "first"
-print "Your second varible is:", "second"
-print "Your thrid valible is:", "third"
+print "The script is called:", script
+print "Your first varible is:", first
+print "Your second varible is:", second
+print "Your thrid valible is:", third
--- ex14.py
+++ ex14.py
@@ -1,21 +1,21 @@
-"From sys import argv"
+from sys import argv
 
-"script, user_name = argv "
-"promit = '> '"
+script, user_name = argv
+prompt = '> '
 
 print "Hi %s, I'm the %s script."
 print "I'd like to ask you a few questions."
-print "Do you like me %s?"
-likes = raw_input()
+print "Do you like me %s?" % user_name
+likes = raw_input(prompt)
 
-print "Where do you live %s?"
-lives = raw_input()
+print "Where do you live %s?" % user_name
+lives = raw_input(prompt)
 
 print "What kind of computer do you have?"
-computer = raw_input()
+computer = raw_input(prompt)
 
 print ""
-"Alright, so you said %r abut liking me."
-"You live in %r. Not sure where that is."
-"And you have a %r computer. Nice."
-" % (likes, lives, computer)"  
+Alright, so you said %r about liking me.
+You live in %r. Not sure where that is.
+And you have a %r computer. Nice.
+"" % (likes, lives, computer)
